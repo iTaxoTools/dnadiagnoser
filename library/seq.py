a2b11d6e5f0a4babdad1d82f02dbcaf95f844be7
Vladimir Kharchev <necrosovereign@gmail.com>
2020-10-28 17:02:52 +0100

    Basic operations on Seq

--- library/seq.py
+++ library/seq.py
@@ -1,3 +1,5 @@
+from typing import Union, Iterator, Tuple
+import numpy as np
 from functools import reduce
 seq_read_dict = dict(
     A=1,
@@ -18,10 +20,10 @@ seq_read_dict = dict(
 )
 
 seq_set_dict = dict(
-    A={'A'},
-    C={'C'},
-    G={'G'},
-    T={'T'},
+    A=frozenset('A'),
+    C=frozenset('C'),
+    G=frozenset('G'),
+    T=frozenset('T'),
     R=frozenset("AG"),
     Y=frozenset("CT"),
     S=frozenset("GC"),
@@ -41,5 +43,45 @@ assert(all(seq_read_dict[key] == reduce(lambda acc, x: acc |
                                         seq_read_dict[x], seq_set_dict[key], 0) for key in seq_read_dict.keys()))
 
 
-seq_write_list = ["-"] + [char for char,
-                          _ in sorted(seq_read_dict.items(), key=lambda x: x[1])]
+seq_write_tuple = ("-",) + tuple(char for char,
+                                 _ in sorted(seq_read_dict.items(), key=lambda x: x[1]))
+
+
+class Seq:
+    """
+    Store a sequence in a compact way as np.array of bytes
+
+    Each byte encodes a set of nucleotides into the 4 lower bits with correspondences:
+    A <~> 0b0001
+    C <~> 0b0010
+    G <~> 0b0100
+    T <~> 0b1000
+    """
+
+    def __init__(self, data: np.array) -> None:
+        self.data = data
+
+    @classmethod
+    def from_str(cls, sequence: str) -> 'Seq':
+        seq = cls(np.empty(len(sequence), dtype="byte"))
+        for i, el in enumerate(map(seq_read_dict.get, sequence)):
+            seq.data[i] = el
+        return seq
+
+    def __or__(self, other: 'Seq') -> 'Seq':
+        return Seq(self.data | other.data)
+
+    def __str__(self) -> str:
+        return "".join(map(seq_write_tuple.__getitem__, self.data))
+
+    def __iter__(self) -> Iterator:
+        return self.data.__iter__()
+
+
+def differences(seq1: Seq, seq2: Seq) -> Iterator[Tuple[int, int, int]]:
+    """
+    Returns an iterator over incompatible nucleotides of arguments with indices
+
+    Iterator element: (index, nucleotide1, nucleotide2)
+    """
+    return ((i, nuc1, nuc2) for i, (nuc1, nuc2) in enumerate(zip(seq1, seq2)) if not nuc1 & nuc2 and nuc1 and nuc2)
